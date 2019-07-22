aa072f5137b9353a88117246aab83200a98d284a
Yashasavi Yadav <46300895+yashasavi004@users.noreply.github.com>
2019-07-22 16:55:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,30 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+const connections = [];
+
+io.sockets.on('connection',(socket) => {
+   connections.push(socket);
+   console.log(' %s sockets is connected', connections.length);
+  socket.on('disconnect', () => {
+      connections.splice(connections.indexOf(socket), 1);
+   });
+  socket.on('sending message', (message) => {
+      console.log('Message is received :', message);
+    io.sockets.emit('new message', {message: message});
+   });
+});
+
+/*io.on('connection', function(socket){
+  socket.username = "Test";
+  socket.on('change_username', (data) => {
+    socket.username = data.username;
+  })
+  
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
-});
+});*/
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
